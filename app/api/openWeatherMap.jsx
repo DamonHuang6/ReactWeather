eeb8297d4813337d2c9057cf5fe61a549310debc
yh164 <yh164@duke.edu>
2017-02-04 11:58:26 -0500

    initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+/**
+ * Created by Damon on 2017/2/4.
+ */
+var axios = require("axios");
+
+const OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?appid=466f659c1c2f301f6bf045ad38a68f2e&units=metric";
+
+module.exports = {
+    getTemp: function (location) {
+        var encodeLocation = encodeURIComponent(location);
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodeLocation}`;
+
+        return axios.get(requestUrl).then(function (res) {
+            if (res.data.cod && res.data.message) {
+                throw new Error(res.data.message);
+            } else {
+                return res.data.main.temp;
+            }
+        }, function (res) {
+            throw new Error(res.data.message);
+        })
+    }
+};
